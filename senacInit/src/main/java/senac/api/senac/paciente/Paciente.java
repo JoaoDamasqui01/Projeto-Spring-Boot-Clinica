221ba9b2e92bf6351cb1db2077f8d95ec0a6b86c
João Luiz Damasqui <jldamasqui2003@gmail.com>
2025-09-26 21:32:43 -0300

    Update Paciente.java adicionar atualizar informações

--- senacInit/src/main/java/senac/api/senac/paciente/Paciente.java
+++ senacInit/src/main/java/senac/api/senac/paciente/Paciente.java
@@ -35,6 +35,18 @@ public class Paciente{
         this.endereco = new Endereco(dados.endereco());
     }
 
+    public void atualizarInformacoes(@Valid DadosAtualizacaoPaciente dados){
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarEndereco(dados.endereco());
+        }
+    }
 
 
 }
+
